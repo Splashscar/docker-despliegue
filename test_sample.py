ff18e8a5a986c6f130037fcec78c52776fc079e2
Splashscar <felipemendieta389@gmail.com>
2026-08-25 09:59:52 -0500

    Fix prueba fallo intencional

--- test_sample.py
+++ test_sample.py
@@ -3,4 +3,4 @@ from app import sample
 def test_ruta_principal():
     cliente = sample.test_client()
     respuesta = cliente.get("/")
-    assert respuesta.status_code == 404
+    assert respuesta.status_code == 200
